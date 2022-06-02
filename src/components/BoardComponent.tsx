1b94c215984ae9178471f440c9bf1beabf9d3916
Andrew Snisar <andrew.snisar@gmail.com>
2022-06-02 22:55:26 +0300

    made highlighting cells and available cells, made ability to move figures

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -3,6 +3,7 @@ import React, { FC } from 'react';
 import CellComponent from './CellComponent';
 
 import { Board } from '../models/Board';
+import { Cell } from '../models/Cell';
 
 interface BoardProps {
   board: Board;
@@ -10,12 +11,42 @@ interface BoardProps {
 }
 
 const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+  const [selectedCell, setSelectedCell] = React.useState<Cell | null>(null);
+
+  function clickCell(cell: Cell) {
+    if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
+      selectedCell.moveFigure(cell);
+      setSelectedCell(null);
+    } else {
+      setSelectedCell(cell);
+    }
+  }
+
+  function highlightCells() {
+    board.highlightCells(selectedCell);
+    updateBoard();
+  }
+
+  function updateBoard() {
+    const newBoard = board.getCopyBoard();
+    setBoard(newBoard);
+  }
+
+  React.useEffect(() => {
+    highlightCells();
+  }, [selectedCell]);
+
   return (
     <div className="board">
       {board.cells.map((row, i) => (
         <React.Fragment key={i}>
           {row.map(cell => (
-            <CellComponent cell={cell} key={cell.id} />
+            <CellComponent
+              key={cell.id}
+              cell={cell}
+              selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+              onClick={clickCell}
+            />
           ))}
         </React.Fragment>
       ))}
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -4,11 +4,18 @@ import { Cell } from '../models/Cell';
 
 interface CellProps {
   cell: Cell;
+  selected: boolean;
+  onClick: (cell: Cell) => void;
 }
 
-const CellComponent: FC<CellProps> = ({ cell }) => {
+const CellComponent: FC<CellProps> = ({ cell, selected, onClick }) => {
   return (
-    <div className={['cell', cell.color].join(' ')}>
+    <div
+      className={['cell', cell.color, selected && 'selected'].join(' ')}
+      onClick={() => onClick(cell)}
+      style={{ background: cell.available && cell.figure ? 'green' : '' }}
+    >
+      {cell.available && !cell.figure && <div className="available" />}
       {cell.figure?.logo && <img src={cell.figure.logo} alt={cell.figure.name} />}
     </div>
   );
